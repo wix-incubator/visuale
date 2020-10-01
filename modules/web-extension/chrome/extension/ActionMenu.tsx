a0cd9e9c4e613dcaf0414a90c68075b1ee7ed7c2
Dean Shub <deanshub@gmail.com>
2020-10-01 10:33:13 +0300

    Nissix "optionalChaining" plugin

--- modules/web-extension/chrome/extension/ActionMenu.tsx
+++ modules/web-extension/chrome/extension/ActionMenu.tsx
@@ -1,47 +1,36 @@
-import React from 'react';
-
+import React from "react";
 interface IArg {
-  name: string;
-  type: 'string' | 'generic' | 'number' | 'boolean';
-}
-
-export interface IAction {
-  name: string;
-  args: IArg[];
-}
-
-export interface IActionMenuProps {
-  component: {
     name: string;
-    actions: IAction[]
-  };
+    type: "string" | "generic" | "number" | "boolean";
 }
-
+export interface IAction {
+    name: string;
+    args: IArg[];
+}
+export interface IActionMenuProps {
+    component: {
+        name: string;
+        actions: IAction[];
+    };
+}
 export class ActionMenu extends React.Component<IActionMenuProps> {
-  public render() {
-    const {component} = this.props;
-    return (
-      <div id="popper" style={{background: '#ddd', padding: '10px'}} data-component="_">
+    public render() {
+        const { component } = this.props;
+        return (<div id="popper" style={{ background: "#ddd", padding: "10px" }} data-component="_">
         <h3>{component.name}</h3>
-        {component.actions.map((action: IAction) =>
-          <button onClick={() => this.recordAction(action.name)} key={action.name}>{action.name}</button>,
-        )}
-      </div>
-    );
-  }
-
-  private recordAction = (actionName: string) => {
-    const {component} = this.props;
-    const action = component.actions.find((anAction: IAction) => anAction.name === actionName);
-    if (!action) {
-      return;
+        {component.actions.map((action: IAction) => <button onClick={() => this.recordAction(action.name)} key={action.name}>{action.name}</button>)}
+      </div>);
     }
-    let parameters: string[] = [];
-
-    if (action.args.length) {
-      parameters = action.args.map((arg) =>
-        (prompt(`${arg.name}: ${arg.type}`)) || '');
-    }
-    console.log(`call ${component.name}.${actionName} with parameters ${JSON.stringify(parameters)}`);
-  };
+    private recordAction = (actionName: string) => {
+        const { component } = this.props;
+        const action = component.actions.find((anAction: IAction) => anAction.name === actionName);
+        if (!action) {
+            return;
+        }
+        let parameters: string[] = [];
+        if (action.args.length) {
+            parameters = action.args.map((arg) => (prompt(`${arg.name}: ${arg.type}`)) || "");
+        }
+        console.log(`call ${component.name}.${actionName} with parameters ${JSON.stringify(parameters)}`);
+    };
 }
--- modules/web-extension/chrome/extension/contentScript.tsx
+++ modules/web-extension/chrome/extension/contentScript.tsx
@@ -1,120 +1,105 @@
-import Popper from 'popper.js';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {ActionMenu, IAction} from './ActionMenu';
-
-const VISUALE_CONTAINER_ID = '--visuale-container';
-
+import Popper from "popper.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import { ActionMenu, IAction } from "./ActionMenu";
+const VISUALE_CONTAINER_ID = "--visuale-container";
 export interface IComponent {
-  name: string;
-  actions: IAction[];
+    name: string;
+    actions: IAction[];
 }
-
 const componentMap: Record<string, IComponent> = {
-  MyComponent: {
-    name: 'MyComponent',
-    actions: [{
-      name: 'enterText',
-      args: [{
-        name: 'text',
-        type: 'string',
-      }],
+    MyComponent: {
+        name: "MyComponent",
+        actions: [{
+                name: "enterText",
+                args: [{
+                        name: "text",
+                        type: "string"
+                    }]
+            },
+            {
+                name: "clickButton",
+                args: []
+            },
+        ]
     },
-      {
-        name: 'clickButton',
-        args: [],
-      },
-    ],
-  },
-  ChildComponent: {
-    name: 'ChildComponent',
-    actions: [{
-      name: 'select',
-      args: [{
-        name: 'index',
-        type: 'number',
-      }, {
-        name: 'isValid',
-        type: 'boolean',
-      }],
-    }],
-  },
+    ChildComponent: {
+        name: "ChildComponent",
+        actions: [{
+                name: "select",
+                args: [{
+                        name: "index",
+                        type: "number"
+                    }, {
+                        name: "isValid",
+                        type: "boolean"
+                    }]
+            }]
+    }
 };
-
 function registerInspector() {
-  function findReactComponentName(instance: any): string {
-    if (instance.return && instance.return.elementType && !instance.return.elementType.name) {
-      return findReactComponentName(instance.return);
+    function findReactComponentName(instance: any): string {
+        if (instance.return?.elementType && !instance.return.elementType.name) {
+            return findReactComponentName(instance.return);
+        }
+        return instance.return.elementType && instance.return.elementType.name;
     }
-    return instance.return.elementType && instance.return.elementType.name;
-  }
-
-  function findDOMComponentName(el: HTMLElement | null): string | null {
-    if (!el) {
-      return null;
+    function findDOMComponentName(el: HTMLElement | null): string | null {
+        if (!el) {
+            return null;
+        }
+        const componentName = el && el.getAttribute("data-component");
+        if (componentName === "_") {
+            return null;
+        }
+        if (componentName) {
+            return componentName;
+        }
+        return findDOMComponentName(el.parentElement);
     }
-    const componentName = el && el.getAttribute('data-component');
-    if (componentName === '_') {
-      return null;
+    function findReactInstance(el: HTMLElement) {
+        let instance;
+        for (const key of Object.keys(el)) {
+            if (key.startsWith("__reactInternalInstance$")) {
+                instance = (el as any)[key];
+                break;
+            }
+        }
+        if (!instance) {
+            return;
+        }
+        return findReactComponentName(instance);
     }
-    if (componentName) {
-      return componentName;
-    }
-    return findDOMComponentName(el.parentElement);
-  }
-
-  function findReactInstance(el: HTMLElement) {
-    let instance;
-    for (const key of Object.keys(el)) {
-      if (key.startsWith('__reactInternalInstance$')) {
-        instance = (el as any)[key];
-        break;
-      }
-    }
-
-    if (!instance) {
-      return;
-    }
-    return findReactComponentName(instance);
-  }
-
-  let lastElement: HTMLElement | null = null;
-  let lastComponent: IComponent | null = null;
-
-  document.body.addEventListener('click', (e) => {
-    const {target} = e;
-    if (!target || lastElement === target) {
-      return;
-    }
-    lastElement = target as HTMLElement;
-
-    const name = findDOMComponentName(lastElement);
-
-    if (!name || !componentMap[name]) {
-      return;
-    }
-
-    const component = componentMap[name];
-    if (lastComponent === component) {
-      return;
-    }
-    lastComponent = component;
-
-    console.log(`these are the available actions for ${name}`, component.actions.map((action: IAction) => action.name));
-    ReactDOM.render(<ActionMenu component={component}/>, document.getElementById(VISUALE_CONTAINER_ID));
-    const popper = new Popper(lastElement, document.getElementById(VISUALE_CONTAINER_ID) as Element, {
-      placement: 'right',
-      modifiers: {
-        offset: {
-          offset: '0px, 10px',
-        },
-      },
+    let lastElement: HTMLElement | null = null;
+    let lastComponent: IComponent | null = null;
+    document.body.addEventListener("click", (e) => {
+        const { target } = e;
+        if (!target || lastElement && target) {
+            return;
+        }
+        lastElement = target as HTMLElement;
+        const name = findDOMComponentName(lastElement);
+        if (!name || !componentMap[name]) {
+            return;
+        }
+        const component = componentMap[name];
+        if (lastComponent && component) {
+            return;
+        }
+        lastComponent = component;
+        console.log(`these are the available actions for ${name}`, component.actions.map((action: IAction) => action.name));
+        ReactDOM.render(<ActionMenu component={component}/>, document.getElementById(VISUALE_CONTAINER_ID));
+        const popper = new Popper(lastElement, document.getElementById(VISUALE_CONTAINER_ID) as Element, {
+            placement: "right",
+            modifiers: {
+                offset: {
+                    offset: "0px, 10px"
+                }
+            }
+        });
     });
-  });
 }
-
-const container = document.createElement('div');
+const container = document.createElement("div");
 container.id = VISUALE_CONTAINER_ID;
 document.body.appendChild(container);
-
 registerInspector();
